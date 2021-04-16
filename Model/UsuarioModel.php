7b838ce2c0b24d52947089882944f255d43fd1f6
Guzz-drk <182740@upf.br>
2021-04-16 20:28:19 -0300

    Commit Cleiton

---
+++ Model/UsuarioModel.php
@@ -0,0 +1,24 @@
+<?php 
+
+class Usuario{
+    var $id;
+    var $nome;
+    var $cpf;
+    var $rg;
+    var $telefone;
+    var $dataNascimento;
+    var $email;
+    var $rua;
+    var $bairro;
+    var $numero;
+    var $cidade;
+    var $senha;
+    var $controleAcesso;
+}
+
+
+
+
+
+
+?>
